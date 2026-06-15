1e1795d3171bb002f382bd59d089a618c6f5c930
uriel <uri.academico.red@gmail.com>
2026-06-14 23:41:04 -0600

    Algoritmo de detección de imagenes ajustado para fotos blanco y negro

--- control/image_parser.py
+++ control/image_parser.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 class ImgMeca():
     
@@ -35,8 +37,17 @@ class ImgMeca():
                                )
     
     def bordes(self):
-        self.bordes=cv2.Canny(self.gris,50,150)
-    
+        _, self.bordes = cv2.threshold(
+        self.gris,
+        180,
+        255,
+        cv2.THRESH_BINARY_INV
+    )
+    def adelgazar(self):
+
+        self.edge_img = cv2.ximgproc.thinning(
+            self.edge_img
+        )
     def pixel_a_mm(self,x,y):
         l,a=self.img.shape[:2]
         X=x*self.ANCHO/a
@@ -44,18 +55,124 @@ class ImgMeca():
         return (float(X),float(Y))
 
     def vectoriza(self):
-        contornos,_=cv2.findContours(
-                self.bordes,
-                cv2.RETR_EXTERNAL,
-                cv2.CHAIN_APPROX_NONE
-                    )
+        contornos,_ = cv2.findContours(
+            self.bordes,
+            cv2.RETR_LIST,
+            cv2.CHAIN_APPROX_NONE
+        )
+
         self.vector=[]
+
+
         for c in contornos:
+
+            area = cv2.contourArea(c)
+
+            if area < 50:
+                continue
+
+
             linea=[]
+
             for punto in c:
-                x,y=punto[0]
+
+                x,y = punto[0]
+
                 linea.append(
-                            self.pixel_a_mm(x,y)
-                        )
+                    self.pixel_a_mm(x,y)
+                )
+
             self.vector.append(linea)
 
+    def relleno_morfologico(self):
+
+        kernel = np.ones((3,3), np.uint8)
+
+        self.gris = cv2.morphologyEx(
+            self.gris,
+            cv2.MORPH_CLOSE,
+            kernel,
+            iterations=2
+        )
+
+    def simplifica(self, tolerancia=2):
+
+        nuevo_vector=[]
+
+        for contorno in self.vector:
+
+            puntos = np.array(contorno, dtype=np.float32)
+
+            aprox = cv2.approxPolyDP(
+                puntos,
+                tolerancia,
+                False
+            )
+
+            linea=[]
+
+            for punto in aprox:
+                x,y = punto[0]
+                linea.append(
+                    (float(x),float(y))
+                )
+
+            nuevo_vector.append(linea)
+
+
+        self.vector = nuevo_vector
+
+    def preview(self):
+
+        plt.figure()
+
+        for linea in self.vector:
+
+            x=[]
+            y=[]
+
+            for p in linea:
+                x.append(p[0])
+                y.append(p[1])
+
+            plt.plot(x,y)
+
+
+        plt.gca().invert_yaxis()
+        plt.axis("equal")
+        plt.show()
+
+    def genera_trayectoria(self):
+
+        self.trayectoria=[]
+
+
+        for linea in self.vector:
+
+            if len(linea)==0:
+                continue
+
+
+            inicio=linea[0]
+
+
+            self.trayectoria.append(
+                ("LAPIZ ARRIBA",inicio)
+            )
+
+
+            self.trayectoria.append(
+                ("LAPIZ ABAJO",inicio)
+            )
+
+
+            for punto in linea[1:]:
+
+                self.trayectoria.append(
+                    ("MUEVE",punto)
+                )
+
+
+            self.trayectoria.append(
+                ("LAPIZ ARRIBA",linea[-1])
+            )
--- control/main.py
+++ control/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE = Path(__file__).resolve().parent.parent
 
-imagen = BASE / "furrul_prueba.png"
+imagen = BASE / "furrul_negro.png"
 print(BASE)
 print(imagen)
 print(imagen.exists())
@@ -12,7 +12,10 @@ rbs=ImgMeca(200,200)
 rbs.carga_img(imagen)
 rbs.normaliza()
 rbs.blanco_negro()
+rbs.relleno_morfologico()
 rbs.bordes()
 rbs.vectoriza()
 #print(rbs.vector)
 print(len(rbs.vector))
+rbs.simplifica(1.2)
+rbs.preview()
